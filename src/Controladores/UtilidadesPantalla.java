23a16a09e3ceae76c41a9406bf35f9741c2c0adc
Ismael <Ivii95@users.noreply.github.com>
2019-10-10 19:34:40 +0200

    Commit 10/10/2019
    Trabajo terminada vista formulario

--- src/Controladores/UtilidadesPantalla.java
+++ src/Controladores/UtilidadesPantalla.java
@@ -8,6 +8,8 @@ package Controladores;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -38,5 +40,22 @@ public class UtilidadesPantalla {
         }
         return hsURL;
     }
+    private static final char[] CONSTS_HEX = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
+    public static String encriptaEnMD5(String stringAEncriptar) {
+        try {
+            MessageDigest msgd = MessageDigest.getInstance("MD5");
+            byte[] bytes = msgd.digest(stringAEncriptar.getBytes());
+            StringBuilder strbCadenaMD5 = new StringBuilder(2 * bytes.length);
+            for (int i = 0; i < bytes.length; i++) {
+                int bajo = (int) (bytes[i] & 0x0f);
+                int alto = (int) ((bytes[i] & 0xf0) >> 4);
+                strbCadenaMD5.append(CONSTS_HEX[alto]);
+                strbCadenaMD5.append(CONSTS_HEX[bajo]);
+            }
+            return strbCadenaMD5.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
 }
